956495b565585341105c6c63e7adc2833a99e0f9
TedyIsBack <t.petkova@proxiad.com>
2025-11-22 01:37:09 +0200

    Fix QR code generating and update design

--- ExploreNow/app/src/main/java/com/example/explorenow/LandmarkListActivity.java
+++ ExploreNow/app/src/main/java/com/example/explorenow/LandmarkListActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,7 +16,6 @@ import com.google.android.material.button.MaterialButton;
 
 public class LandmarkListActivity extends AppCompatActivity {
 
-    public static final String LANDMARK_ID = "landmark_id";
     private LandmarkViewModel viewModel;
     private LandmarkAdapter adapter;
 
@@ -28,26 +26,17 @@ public class LandmarkListActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerViewLandmarks);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new LandmarkAdapter();
+        recyclerView.setAdapter(adapter);
 
         TextView tvEmpty = findViewById(R.id.tvEmpty);
         MaterialButton btnAdd = findViewById(R.id.btnAddLandmark);
 
-        adapter = new LandmarkAdapter();
-        recyclerView.setAdapter(adapter);
-
-//        adapter.setOnItemClick(landmark -> {
-//            Intent intent = new Intent(this, LandmarkDetailActivity.class);
-//            intent.putExtra(LANDMARK_ID, landmark.id);
-//            startActivity(intent);
-//        });
-
         adapter.setOnQrClick(landmark -> {
             Intent intent = new Intent(this, LandmarkQrActivity.class);
-            intent.putExtra(LANDMARK_ID, landmark.id);
+            intent.putExtra(LandmarkQrActivity.LANDMARK_ID, landmark.id);
             startActivity(intent);
         });
-
-
 
         btnAdd.setOnClickListener(v -> {
             Intent intent = new Intent(this, EditorActivity.class);
@@ -56,22 +45,16 @@ public class LandmarkListActivity extends AppCompatActivity {
 
         viewModel = new ViewModelProvider(this).get(LandmarkViewModel.class);
         viewModel.getAllLandmarks().observe(this, landmarks -> {
-                    if (landmarks == null || landmarks.isEmpty()) {
-                        tvEmpty.setVisibility(View.VISIBLE);
-                        btnAdd.setVisibility(View.VISIBLE);
-                        recyclerView.setVisibility(View.GONE);
-                    } else {
-                        tvEmpty.setVisibility(View.GONE);
-                        btnAdd.setVisibility(View.GONE);
-                        recyclerView.setVisibility(View.VISIBLE);
-                        adapter.submitList(landmarks);
-                    }
-                });
-
-        adapter.setOnQrClick(landmark -> {
-            Toast.makeText(this, "Generate QR for " + landmark.name, Toast.LENGTH_SHORT).show();
+            if (landmarks == null || landmarks.isEmpty()) {
+                tvEmpty.setVisibility(View.VISIBLE);
+                btnAdd.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+            } else {
+                tvEmpty.setVisibility(View.GONE);
+                btnAdd.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                adapter.submitList(landmarks);
+            }
         });
-
-        viewModel.getAllLandmarks().observe(this, landmarks -> adapter.submitList(landmarks));
     }
 }
